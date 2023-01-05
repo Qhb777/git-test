bfa60b5f914cf362d06b8e1664efed7ea0433b17
Qhb <Qhbzzz777@163.com>
2023-01-05 22:51:42 +0800

    测试提交

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,16 @@
+package com.atguigu.git;
+
+/**
+ * ClassName: GitTest
+ * Package: com.atguigu.git
+ * Description:
+ *
+ * @Author Qhb
+ * @Create 2023/1/5 22:45
+ * @Version 1.0
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
